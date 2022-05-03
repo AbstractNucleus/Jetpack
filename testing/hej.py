d16d58771874179fbf6c9d1d34c927dc900f8ea2
AbstractNucleus <noelhkleen@gmail.com>
2022-05-03 09:53:44 +0200

    .

--- testing/hej.py
+++ testing/hej.py
@@ -8,9 +8,10 @@ def init():
     global run
     scene = canvas(width=900, height=500, autoscale=False)
     run = True
-    misc()
-    objects()
-    velocities()
+
+    init_misc()
+    init_objects()
+    init_velocities()
 
 
 def exit():
@@ -19,49 +20,50 @@ def exit():
     os.kill(os.getpid(), signal.SIGTERM)
 
 
-def misc():
-    exit_button = button(bind=exit, text="Quit")
-    
-
-def objects():
+def init_misc():
     global keys_pressed
+
+    keys_pressed = wtext(pos=scene.caption_anchor)
+    exit_button = button(bind=exit, text="Quit")
+
+
+def init_objects():
     global paddle_1
     global paddle_2
     global ball
 
-    keys_pressed = wtext(pos=scene.caption_anchor)
-    ball = sphere(pos=vector(0, 0, 0), radius=0.02)
-    paddle_1 = box(pos=vector(0, -10, 0), axis=vector(0,0,0), size=vector(5, 1, 0))
-    paddle_2 = box(pos=vector(0, 10, 0), axis=vector(0,0,0), size=vector(5, 1, 0))
+    ball = sphere(pos=vector(0, 0, 0), radius=1,
+                  color=vector(254, 254, 254))
+    paddle_1 = box(pos=vector(0, -10, 0), size=vector(5, 1, 0))
+    paddle_2 = box(pos=vector(0, 10, 0), size=vector(5, 1, 0))
 
 
-def velocities():
-    global paddle_1_vel
-    global paddle_2_vel
-    global ball_vel
-
-    paddle_1_vel = vector(0, 0, 0)
-    paddle_2_vel = vector(0, 0, 0)
-    ball_vel = vector(0, 0, 0)
+def init_velocities():
+    paddle_1.vel = vector(0, 0, 0)
+    paddle_2.vel = vector(0, 0, 0)
+    ball.vel = vector(0, 0, 0)
 
 
 def main():
     keys = keysdown()
     keys_pressed.text = keys
 
-    #print(paddle_1.pos)
-    if paddle_1.pos.x == 15: paddle_1_vel.x = 0
+    if (paddle_1.pos.x > 15) or (paddle_1.pos.x < -15):
+        paddle_1.vel.x = 0
+    if (paddle_2.pos.x > 15) or (paddle_2.pos.x < -15):
+        paddle_2.vel.x = 0
 
-    if "a" in keys: paddle_1_vel.x = -0.2
-    if "d" in keys: paddle_1_vel.x = 0.2
-    if "left" in keys: paddle_2_vel.x = -0.2
-    if "right" in keys: paddle_2_vel.x = 0.2
-    
+    if "a" in keys:
+        paddle_1.vel.x = -0.2
+    if "d" in keys:
+        paddle_1.vel.x = 0.2
+    if "left" in keys:
+        paddle_2.vel.x = -0.2
+    if "right" in keys:
+        paddle_2.vel.x = 0.2
 
-
-
-    paddle_1.pos += paddle_1_vel
-    paddle_2.pos += paddle_2_vel
+    paddle_1.pos += paddle_1.vel
+    paddle_2.pos += paddle_2.vel
 
 
 if __name__ == "__main__":
@@ -74,8 +76,3 @@ if __name__ == "__main__":
     except:
         print("ERROR")
         exit()
-
-
-    
-
-
